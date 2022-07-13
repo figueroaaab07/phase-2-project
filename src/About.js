0074c3daa3f63c3cf5aff920b14e5f784ab97ecf
Alejandro Luis Figueroa Cevallos <figueroaaab07@gmail.com>
2022-07-13 12:52:28 -0700

    Add Perseverance Rover Photos

--- src/About.js
+++ src/About.js
@@ -6,9 +6,10 @@ function About() {
   const curiosity = require('./images/curiosity.png'); // with require
   const perseverance = require('./images/perseverance.png'); // with require
   const cameras = require('./images/rover-cameras.png'); // with require
+  const perseverance_cam_desc = require('./images/perseverance-cameras.jpg'); // with require
   const curiosity_cam = require('./images/curiositycameras2.jpg'); // with require
   const perseverance_cam = require('./images/mars_2020_cameras_labeled_web-full2.jpg'); // with require
-  const landing_cam = require('./images/EDLCam2-web.jpg'); // with require
+  // const landing_cam = require('./images/EDLCam2-web.jpg'); // with require
 
   return (
     <>
@@ -22,10 +23,10 @@ function About() {
           <img id="curiosity-cameras" alt="Curiosity Cameras" src={curiosity_cam} width="100%"/> 
         </div>
         <div className="column-rover-cameras">
-          <img id="perseverance-cameras" alt="Perseverance Cameras" src={perseverance_cam} width="100%"/> 
+          <img id="perseverance-cameras-desc" alt="Perseverance Cameras Desc" src={perseverance_cam_desc} height="80%" width="100%"/> 
         </div>
         <div className="column-rover-cameras">
-          <img id="landing-cameras" alt="Landing Cameras" src={landing_cam} width="100%"/> 
+          <img id="perseverance-cameras" alt="Perseverance Cameras" src={perseverance_cam} width="100%"/> 
         </div>
       </div>
       <h2>Rover Misions</h2>
